35e474fc31d3d4383e4c2b1c9003b4f1c326f3f9
renn72 <simplesam8856@gmail.com>
2024-08-15 12:26:37 +1000

    expand entry model for all round orders

---
+++ drizzle/0004_known_secret_warriors.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `pb_comp_entry` ADD `is_squat_four` integer;--> statement-breakpoint
+ALTER TABLE `pb_comp_entry` ADD `is_bench_four` integer;--> statement-breakpoint
+ALTER TABLE `pb_comp_entry` ADD `is_deadlift_four` integer;
