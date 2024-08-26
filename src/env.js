77cfb0f01589e5f45953219ccf7a0483907a4775
renn72 <simplesam8856@gmail.com>
2024-08-26 11:18:34 +1000

    fix env

--- src/env.js
+++ src/env.js
@@ -2,64 +2,64 @@ import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const env = createEnv({
-    /**
-     * Specify your server-side environment variables schema here. This way you can ensure the app
-     * isn't built with invalid env vars.
-     */
-    server: {
-        DATABASE_URL: z.string().url(),
-        CLERK_SECRET_KEY: z.string(),
-        TURSO_DATABASE_URL: z.string().url(),
-        TURSO_AUTH_TOKEN: z.string(),
-        PUSHER_APP_ID: z.string(),
-        PUSHER_KEY: z.string(),
-        PUSHER_SECRET: z.string(),
-        PUSHER_CLUSTER: z.string(),
-        NODE_ENV: z
-            .enum(['development', 'test', 'production'])
-            .default('development'),
-    },
+  /**
+   * Specify your server-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars.
+   */
+  server: {
+    DATABASE_URL: z.string().url(),
+    CLERK_SECRET_KEY: z.string(),
+    TURSO_DATABASE_URL: z.string().url(),
+    TURSO_AUTH_TOKEN: z.string(),
+    PUSHER_APP_ID: z.string(),
+    PUSHER_KEY: z.string(),
+    PUSHER_SECRET: z.string(),
+    PUSHER_CLUSTER: z.string(),
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('development'),
+  },
 
-    /**
-     * Specify your client-side environment variables schema here. This way you can ensure the app
-     * isn't built with invalid env vars. To expose them to the client, prefix them with
-     * `NEXT_PUBLIC_`.
-     */
-    client: {
-        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-        NEXT_PUBLIC_PUSHER_KEY: z.string(),
-        NEXT_PUBLIC_PUSHER_CLUSTER: z.string(),
-        // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    },
+  /**
+   * Specify your client-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars. To expose them to the client, prefix them with
+   * `NEXT_PUBLIC_`.
+   */
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_PUSHER_KEY: z.string(),
+    NEXT_PUBLIC_PUSHER_CLUSTER: z.string(),
+    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  },
 
-    /**
-     * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-     * middlewares) or client-side so we need to destruct manually.
-     */
-    runtimeEnv: {
-        DATABASE_URL: process.env.DATABASE_URL,
-        NODE_ENV: process.env.NODE_ENV,
-        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-            process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-        TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
-        TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
-        NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
-        NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-        PUSHER_APP_ID: process.env.PUSHER_APP_ID,
-        PUSHER_KEY: process.env.PUSHER_KEY,
-        PUSHER_SECRET: process.env.PUSHER_SECRET,
-        PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
-        // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-    },
-    /**
-     * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-     * useful for Docker builds.
-     */
-    skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-    /**
-     * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-     * `SOME_VAR=''` will throw an error.
-     */
-    emptyStringAsUndefined: true,
+  /**
+   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
+   * middlewares) or client-side so we need to destruct manually.
+   */
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
+    TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
+    NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
+    NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+    PUSHER_KEY: process.env.PUSHER_KEY,
+    PUSHER_SECRET: process.env.PUSHER_SECRET,
+    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  },
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  /**
+   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
+   * `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
 })
