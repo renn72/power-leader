9aa1c657e68b0e95a9c305b2e1b62f3bf4fe7f13
renn72 <simplesam8856@gmail.com>
2024-08-27 21:34:54 +1000

    fix pusher binding

--- src/app/comp-day/[comp]/_components/comp-table.tsx
+++ src/app/comp-day/[comp]/_components/comp-table.tsx
@@ -4,6 +4,9 @@ export const dynamic = 'force-dynamic'
 
 import { api } from '~/trpc/react'
 
+import { pusherClient } from '~/lib/pusher'
+import Pusher from 'pusher-js'
+
 import { toast } from 'sonner'
 import { ScrollArea } from '~/components/ui/scroll-area'
 import {
@@ -40,6 +43,54 @@ const CompTable = ({
       toast(JSON.stringify(e))
     },
   })
+
+  // useEffect(() => {
+  //   Pusher.logToConsole = true
+  //   const channel = pusherClient.subscribe('competition-' + competitonUuid)
+  //
+  //   channel.bind(
+  //     'judge',
+  //     (data: {
+  //       id: number
+  //       entryId: number
+  //       judge: number
+  //       isGood: boolean
+  //     }) => {
+  //       if (!competition) {
+  //         return
+  //       }
+  //       console.log('data', data)
+  //       ctx.competition.get.setData(competition.id, {
+  //         ...competition,
+  //         entries: competition.entries.map((entry) => {
+  //           return {
+  //             ...entry,
+  //             lift: entry.lift.map((i) => {
+  //               return {
+  //                 ...i,
+  //                 isGoodOne:
+  //                   i.id === data.id && data.judge === 1
+  //                     ? data.isGood
+  //                     : i.isGoodOne,
+  //                 isGoodTwo:
+  //                   i.id === data.id && data.judge === 2
+  //                     ? data.isGood
+  //                     : i.isGoodTwo,
+  //                 isGoodThree:
+  //                   i.id === data.id && data.judge === 3
+  //                     ? data.isGood
+  //                     : i.isGoodThree,
+  //               }
+  //             }),
+  //           }
+  //         }),
+  //       })
+  //     },
+  //   )
+  //   return () => {
+  //     pusherClient.unsubscribe('competition-' + competitonUuid)
+  //   }
+  // }, [competition])
 
   return (
     <div className='rounded-md border border-input p-2'>
--- src/app/comp-day/[comp]/page.tsx
+++ src/app/comp-day/[comp]/page.tsx
@@ -5,8 +5,6 @@ export const dynamic = 'force-dynamic'
 import { useState, useEffect } from 'react'
 
 import { api } from '~/trpc/react'
-import { pusherClient } from '~/lib/pusher'
-import Pusher from 'pusher-js'
 
 import {
   Card,
@@ -40,54 +38,6 @@ const Competition = ({ params }: { params: { comp: string } }) => {
       setRound(competition.compDayInfo.round.toString())
     }
   }, [competition])
-
-  // useEffect(() => {
-  //   Pusher.logToConsole = true
-  //   const channel = pusherClient.subscribe('competition-' + competitonUuid)
-  //
-  //   channel.bind(
-  //     'judge',
-  //     (data: {
-  //       id: number
-  //       entryId: number
-  //       judge: number
-  //       isGood: boolean
-  //     }) => {
-  //       if (!competition) {
-  //         return
-  //       }
-  //       console.log('data', data)
-  //       ctx.competition.get.setData(competition.id, {
-  //         ...competition,
-  //         entries: competition.entries.map((entry) => {
-  //           return {
-  //             ...entry,
-  //             lift: entry.lift.map((i) => {
-  //               return {
-  //                 ...i,
-  //                 isGoodOne:
-  //                   i.id === data.id && data.judge === 1
-  //                     ? data.isGood
-  //                     : i.isGoodOne,
-  //                 isGoodTwo:
-  //                   i.id === data.id && data.judge === 2
-  //                     ? data.isGood
-  //                     : i.isGoodTwo,
-  //                 isGoodThree:
-  //                   i.id === data.id && data.judge === 3
-  //                     ? data.isGood
-  //                     : i.isGoodThree,
-  //               }
-  //             }),
-  //           }
-  //         }),
-  //       })
-  //     },
-  //   )
-  //   return () => {
-  //     pusherClient.unsubscribe('competition-' + competitonUuid)
-  //   }
-  // }, [competition])
 
   if (competitionLoading) return <CompTableSkeletion />
   if (!competition) {
