7917684e836882410407115537516480895d4ae3
renn <ren@warner.systems>
2024-08-30 21:09:58 +1000

    add back login

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,9 +3,9 @@ import '~/styles/globals.css'
 import Navbar from '~/app/_components/navbar'
 import Footer from '~/app/_components/footer'
 
-// import { SignInButton, SignIn, SignedOut, UserButton } from '@clerk/nextjs'
-// import { auth, currentUser } from '@clerk/nextjs/server'
-// import { LogIn } from 'lucide-react'
+import { SignInButton, SignIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { auth, currentUser } from '@clerk/nextjs/server'
+import { LogIn } from 'lucide-react'
 
 export const metadata = {
   title: 'Scoreboard',
@@ -18,32 +18,32 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  // const { userId } = auth()
+  const { userId } = auth()
 
-  // if (!userId) {
-  //   return (
-  //     <div
-  //       className={`flex min-h-screen flex-col items-center justify-center `}
-  //     >
-  //       ping
-  //       <SignInButton
-  //         fallbackRedirectUrl={`/admin`}
-  //         signUpForceRedirectUrl={`/admin`}
-  //         mode='modal'
-  //       >
-  //         <div className='flex items-center gap-2 cursor-pointer hover:underline text-xl text-primary/60 hover:text-primary font-bold'>
-  //           <LogIn
-  //             size={36}
-  //             strokeWidth={2}
-  //           />
-  //           Sign In
-  //         </div>
-  //       </SignInButton>
-  //     </div>
-  //   )
-  // }
+  if (!userId) {
+    return (
+      <div
+        className={`flex min-h-screen flex-col items-center justify-center `}
+      >
+        ping
+        <SignInButton
+          fallbackRedirectUrl={`/admin`}
+          signUpForceRedirectUrl={`/admin`}
+          mode='modal'
+        >
+          <div className='flex items-center gap-2 cursor-pointer hover:underline text-xl text-primary/60 hover:text-primary font-bold'>
+            <LogIn
+              size={36}
+              strokeWidth={2}
+            />
+            Sign In
+          </div>
+        </SignInButton>
+      </div>
+    )
+  }
 
-  // const _user = await currentUser()
+  const _user = await currentUser()
 
   return (
     <div className={`flex h-dvh flex-col `}>
