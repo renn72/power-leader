601a9c01ed0a666838503972e14350a2f2933731
renn <renn@warner.systems>
2025-02-19 16:55:13 +1100

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     <section className='relative flex h-[80vh] w-full flex-col items-center justify-center gap-8 overflow-hidden'>
       <div className='relative z-10 max-w-3xl px-4 text-center sm:px-6 lg:px-8'>
         <h1 className='text-4xl font-bold sm:text-5xl lg:text-6xl'>
-          Power Board
+          Power
         </h1>
       </div>
       <div className='inset-0 z-0 flex flex-col items-center justify-center gap-2'>
