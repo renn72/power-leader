b582407534ee4f3d593cb892699d03f5d0e75491
renn <renn@warner.systems>
2025-03-17 12:23:53 +1100

    fix port

--- src/env.js
+++ src/env.js
@@ -18,7 +18,7 @@ export const env = createEnv({
     EMAIL_SERVER_PASSWORD: z.string(),
     EMAIL_SERVER_USER: z.string(),
     EMAIL_SERVER_HOST: z.string(),
-    EMAIL_SERVER_PORT: z.number(),
+    EMAIL_SERVER_PORT: z.string(),
     EMAIL_FROM: z.string(),
     NODE_ENV: z
       .enum(['development', 'test', 'production'])
