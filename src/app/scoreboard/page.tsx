aaa8afb90c715188afb1c0527292b52c3378bb85
renn <renn@warner.systems>
2025-03-21 14:41:21 +1100

    agge filter

--- src/app/scoreboard/page.tsx
+++ src/app/scoreboard/page.tsx
@@ -14,10 +14,59 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 
+const ages =  [
+  {
+    name : 'teen',
+    min : 14,
+    max : 16,
+  },
+  {
+    name : 'sub-junior',
+    min: 17,
+    max: 19,
+
+  },
+  {
+    name : 'junior',
+    min: 20,
+    max: 23,
+  },
+  {
+    name : 'open',
+    min: 24,
+    max: 39,
+  },
+  {
+    name : 'm1',
+    min: 40,
+    max: 49,
+  },
+  {
+    name : 'm2',
+    min: 50,
+    max: 59,
+  },
+  {
+    name : 'm3',
+    min: 60,
+    max: 69,
+  },
+  {
+    name : 'm4',
+    min: 70,
+    max: 79,
+  },
+  {
+    name : 'm5',
+    min: 80,
+    max: 999,
+  },
+]
 const Board = () => {
   const [table, setTable] = useState('all')
   const [gender, setGender] = useState('')
   const [wc, setWC] = useState('')
+  const [age, setAge] = useState('')
 
   const g = gender ? gender : null
   const ctx = api.useUtils()
@@ -29,7 +78,7 @@ const Board = () => {
 
   return (
     <div className='w-full overflow-hidden'>
-      <div className='px-2 py-1 grid grid-cols-3 max-w-[500px] items-center gap-2 bg-background top-0 left-0 z-[200] w-full'>
+      <div className='px-1 py-1 grid grid-cols-4 max-w-[500px] items-center gap-1 bg-background top-0 left-0 z-[200] w-full'>
         <Select
           value={table}
           onValueChange={(value) => {
@@ -68,7 +117,7 @@ const Board = () => {
             }}
           >
             <SelectTrigger className='w-full h-7'>
-              <SelectValue placeholder='wc' />
+              <SelectValue placeholder='WC' />
             </SelectTrigger>
             <SelectContent>
               <SelectItem
@@ -89,6 +138,30 @@ const Board = () => {
             </SelectContent>
           </Select>
         </Select>
+        <Select
+          value={age}
+          onValueChange={(value) => {
+            setAge(value)
+          }}
+        >
+          <SelectTrigger className='w-full h-7 capitalize'>
+            <SelectValue placeholder='Age' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='all'>All</SelectItem>
+            {ages.map((i) => {
+              return (
+                <SelectItem
+                  key={i.name}
+                  value={i.name}
+                  className='capitalize'
+                >
+                  {i.name}
+                </SelectItem>
+              )
+            })}
+          </SelectContent>
+        </Select>
       </div>
 
       <div className='w-full'>
@@ -97,6 +170,7 @@ const Board = () => {
           table={table}
           gender={g}
           wc={wc}
+          age={age}
         />
       </div>
     </div>
