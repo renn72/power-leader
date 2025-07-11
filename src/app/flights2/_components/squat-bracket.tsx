74813a73ed352f300553b3798e0e8d87206b47fb
renn <renn@warner.systems>
2025-07-12 08:37:28 +1000

    add to other brackets

--- src/app/flights2/_components/squat-bracket.tsx
+++ src/app/flights2/_components/squat-bracket.tsx
@@ -18,6 +18,7 @@ const SquatBracket = ({
 
   return (
     <div className='flex lg:w-full flex-col lg:items-center gap-8'>
+      <div className='text-center text-4xl'>Blackout.wsys.au</div>
       <div className='flex w-full lg:justify-around gap-4'>
         {Array.from(Array(squatBrackets).keys()).map((b) => (
           <Bracket
